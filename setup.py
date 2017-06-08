29a6e3fe8124aebccf1571d97ccbc5963f4fa93d
Arshad Ansari <arshadansari27@gmail.com>
2017-06-08 18:22:10 +0000

    added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from pip.req import parse_requirements
+from setuptools import find_packages, setup
+
+install_reqs = parse_requirements('requirements.txt', session=False)
+reqs = [str(ir.req) for ir in install_reqs]
+
+setup(
+    name='asyncexec',
+    version='1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=reqs,
+)
